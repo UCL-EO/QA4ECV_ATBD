fff1d4b0d6a35261659e599143788260023ef4be
profLewis <dr.p.e.lewis@gmail.com>
2016-03-09 18:51:36 +0000

    updated from QA4ECV_ATBD

--- notebooks/defaults.py
+++ notebooks/defaults.py
@@ -41,3 +41,102 @@ scale = 1.0
 datadir = os.path.abspath('../data')
 print 'data directory',datadir
 
+
+'''
+Codes to pull netcdf files from the server
+
+'''
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    # see https://docs.python.org/2/library/htmlparser.html
+    def handle_starttag(self, tag, attrs):
+        pass;
+        #print "Encountered a start tag:", tag
+    def handle_endtag(self, tag):
+        pass
+        #print "Encountered an end tag :", tag
+    def handle_data(self, data):
+        data = data.strip()
+        if len(data) and data[-1] == '/':
+            # is it a directory?
+            try:
+                self.datadirs.append(data)
+            except:
+                self.datadirs = [data]
+        if len(data) and data.split('.')[-1] == 'nc':
+            # is it a nc file?
+            try:
+                self.ncfiles.append(data)
+            except:
+                self.ncfiles = [data]            
+        
+def getdirs(db):
+    # get directories from url
+    response = urllib2.urlopen(db,None,120)
+    
+    parser = MyHTMLParser()
+    html = response.read()
+    del response
+    parser.feed(html)
+    return parser
+
+def nc_urls(db):
+    '''
+    obtain full list of data urls of nc files
+    '''
+    ncfiles = {}
+    parser = getdirs(db)
+    for sensor in parser.datadirs:
+        ncfiles[sensor] = []
+        parser2 = getdirs(db+sensor)
+        if hasattr(parser2, 'datadirs'):
+            for sub0 in parser2.datadirs:
+                parser3 = getdirs(db+sensor+sub0)
+                if hasattr(parser3, 'datadirs'):
+                    for sub1 in parser3.datadirs:
+                        parser4 = getdirs(db+sensor+sub0+sub1)
+                        if hasattr(parser4, 'datadirs'):
+                            print 'deepest level checked'
+                        if hasattr(parser4, 'ncfiles'):
+                            for nc in parser4.ncfiles:
+                                ncfiles[sensor].append(db+sensor+sub0+sub1+nc)
+                if hasattr(parser3, 'ncfiles'):
+                    for nc in parser3.ncfiles:
+                            ncfiles[sensor].append(db+sensor+sub0+nc)
+        if hasattr(parser2, 'ncfiles'):
+            for nc in parser2.ncfiles:
+                ncfiles[sensor].append(db+sensor+nc)
+    return ncfiles
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+        
+def obtain_data(db,doit=True):
+    print 'connecting to',db
+    ncfiles = nc_urls(db)
+    print 'got urls'
+    nclocals = {}
+    datakeys = ncfiles.keys()
+    for k in datakeys:
+        print k
+        ensure_dir(k)
+        nclocals[k] = []
+        for ncfile in ncfiles[k]:
+            print '.',
+            if doit:
+                data = urllib2.urlopen(ncfile).read()
+            local = ncfile.split('/')[-1]
+            nclocals[k].append(local)
+            # write
+            if doit:
+                f = open(k+local,'wb')
+                f.write(data)
+                f.close()
+    for k in nclocals.keys():
+        if k[-1] == '/':
+            nclocals[k.strip('/')] = nclocals[k]
+            del nclocals[k]
+    return nclocals
+
